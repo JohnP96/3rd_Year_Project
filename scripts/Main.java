d39d75b3f9782f18471ce053396200f7cce2a0a5
JohnP96 <84974743+JohnP96@users.noreply.github.com>
2023-01-02 12:24:01 +0000

    Experimented with midi and tab

--- scripts/Main.java
+++ scripts/Main.java
@@ -1,6 +1,17 @@
+/** Main file for running and testing code */
+
+import javax.sound.midi.*;
+import java.io.File;
+
 public class Main {
-    /* Main file for running and testing code */
     public static void main(String[] args){
 
+        try{
+            Sequence seq = MidiSystem.getSequence(new File("rogero.midi"));
+            System.out.println(seq.getTracks().length);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ scripts/TablatureTranscriptionAlgorithm.java
@@ -0,0 +1,15 @@
+/************************************************************************************
+ * Tablature Transcription Algorithm                                                *
+ *                                                                                  *
+ * Author: John Pederson                                                            *
+ *                                                                                  *
+ * Last edited: 02/01/2023                                                          *
+ *                                                                                  *
+ * Description: Implementation of a genetic algorithm to transcribe guitar          *
+ * tablature from a MIDI file input.                                                *
+ *                                                                                  *
+ * Bug fixes/improvements needed:                                                   *
+ ************************************************************************************/
+public class TablatureTranscriptionAlgorithm {
+
+}
